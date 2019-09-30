abd424f0ca7cf25d872781ef5ea03069accc8874
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-09-30 16:08:56 +0200

    Copied project for simplicity

---
+++ AMT-lab2-counterServlet/src/main/java/ch/heigvd/amt/servlet/CounterServlet.java
@@ -0,0 +1,12 @@
+package ch.heigvd.amt.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Web
+public class CounterServlet {
+}
